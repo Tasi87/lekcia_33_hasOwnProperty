d7dad46e6aebcb1de5659c7f1ac2dcf99e02936a
Tasi <tasi87@gmail.com>
2026-03-17 11:25:37 +0100

    first commit

---
+++ src/script.js
@@ -0,0 +1,18 @@
+//idea	hasOwnProperty
+
+const wizard = {
+	spell: true,
+	makeHardAttack() {
+		return 8
+	},
+	makeSoftAttack() {
+		return 3
+	}
+}
+
+const ordinaryPerson = {
+	firstName: "Delon",
+	makeOrdinaryAttack() {
+		return 1
+	}
+}
